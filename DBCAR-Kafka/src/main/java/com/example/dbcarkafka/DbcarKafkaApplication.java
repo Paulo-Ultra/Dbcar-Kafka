d5424abd76b6f73574cddedfb7ef4e512994d851
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2022-08-10 17:06:08 -0300

    Api Criadda

---
+++ DBCAR-Kafka/src/main/java/com/example/dbcarkafka/DbcarKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbcarkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbcarKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbcarKafkaApplication.class, args);
+	}
+
+}
