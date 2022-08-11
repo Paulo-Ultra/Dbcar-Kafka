0f442cac1daeb605302c6f405dd28e6c70fd6f16
Willian Valentim <williann.valentim@gmail.com>
2022-08-11 16:54:39 -0300

    [feature] Limpeza das classes. Concluída.

--- DBCAR-Kafka/src/main/java/br/com/dbcarkafka/controller/ManutencaoController.java
+++ DBCAR-Kafka/src/main/java/br/com/dbcarkafka/controller/ManutencaoController.java
@@ -1,11 +1,9 @@
 package br.com.dbcarkafka.controller;
 
 import br.com.dbcarkafka.dto.ManutencaoDTO;
-import br.com.dbcarkafka.entity.ManutencaoEntity;
 import br.com.dbcarkafka.enums.StatusManutencao;
 import br.com.dbcarkafka.service.ConsumidorService;
 import lombok.RequiredArgsConstructor;
-import org.apache.kafka.common.protocol.types.Field;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
